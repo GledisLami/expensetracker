ec047d8c13117f690e30beb79d53dac44bde2c10
Gledis <lamigledi@gmail.com>
2023-08-22 17:41:35 +0100

    Reset value of input after submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import './ExpenseForm.css';
 import {useState} from "react";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 
     const[enteredTitle, setEnteredTitle] = useState("");
     const[enteredAmount, setEnteredAmount] = useState("");
@@ -64,23 +64,30 @@ const ExpenseForm = () => {
              amount: enteredAmount,
              date: new Date(enteredDate)
          };
-
-         console.log(expenseData);
+         props.onSaveExpenseData(expenseData);//we execute the function which executes saveExpenseDataHandler, on a different component, it will log for now
+        //on NewExpense we have onSaveExpenseData={saveExpenseDataHandler} as a prop. since ExpenseForm is a child class passed with the prop, we can call it with props.onSave..., to exec
+        //the function inside onSave prop
+        //used to communicate between child-parent. Pass a prop with a function on parent, exec it on child
+         setEnteredTitle('');
+         setEnteredAmount('');
+         setEnteredDate('');
+         //we do this and value={enteredTitle} so when form is submitted value changes to empty
+        //since we dont refresh on submit anymore, two way binding
     };
     return (
         <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler}/>
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                    <input type='date' min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
