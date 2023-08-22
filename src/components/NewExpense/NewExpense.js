fa5fcbdbcfc618200254ca9887d1208e9fa65ae4
Gledis <lamigledi@gmail.com>
2023-08-22 17:42:58 +0100

    Added random id to new expense object

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,20 @@
 import React from "react";
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
-const NewExpense = () => {
+const NewExpense = (props) => {
+
+    const saveExpenseData = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+
+        props.onAddExpense(expenseData); //lifting the state up to the app component, sending data through components
+    }
+
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={saveExpenseData} />
         </div>
     );
 };
